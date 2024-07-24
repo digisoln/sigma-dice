6438e5979bf7ddfb7aa5355922b3cce1100b5792
digisoln <1167079+digisoln@users.noreply.github.com>
2024-07-24 14:01:14 +1000

    Add files via upload
    added total sum of both dice

--- sigma dice.py
+++ sigma dice.py
@@ -1,10 +1,9 @@
 import random
 dice1 = random.randint(1,6)
 dice2 = random.randint(1,6)
-
 if dice1 > dice2:
     score = str(dice1) + str(dice2)
 else:
     score = str(dice2) + str(dice1)
-
-print(score)
+print("Side by side:", score)
+print("Sum of both:", dice1+dice2)
