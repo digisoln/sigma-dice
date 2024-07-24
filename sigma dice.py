e22e6d89cd12ce5cdcd3660750cb3af482ebd060
digisoln <1167079+digisoln@users.noreply.github.com>
2024-07-24 14:17:07 +1000

    Update sigma dice.py
    added goodbye message

--- sigma dice.py
+++ sigma dice.py
@@ -1,9 +1,10 @@
 import random
-dice1 = random.randint(1,9)
-dice2 = random.randint(1,9)
+dice1 = random.randint(1,6)
+dice2 = random.randint(1,6)
 if dice1 > dice2:
     score = str(dice1) + str(dice2)
 else:
     score = str(dice2) + str(dice1)
 print("Side by side:", score)
 print("Sum of both:", dice1+dice2)
+print("goodbye")
